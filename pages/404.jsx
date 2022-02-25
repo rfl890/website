65ec67286432a2b84b23a43acf11b35f58e4e797
RFL890 <87506407+rfl890@users.noreply.github.com>
2022-02-25 01:54:33 +0000

    cleanup

--- pages/404.jsx
+++ pages/404.jsx
@@ -1,25 +1,32 @@
 import Head from "next/head";
-import Link from 'next/link'
-import React, { useState } from "react";
+import Link from "next/link";
 import "inter-ui/inter.css";
 
-export default function Home() {
+export default function Error404() {
   return (
-    <div className="container text-white">
+    <div className="text-white">
       <Head>
         <title>404: Page not found</title>
         <link type="text/plain" rel="author" href="/humans.txt"></link>
         <meta name="viewport" content="width=device-width"></meta>
       </Head>
-	  <h1 className="text-center mt-5 text-7xl font-bold" id="mainHeader">
+      <h1 className="text-center mt-5 text-7xl font-black" id="mainHeader">
         404
       </h1>
-	   <h1 className="text-center mt-2 text-4xl font-bold" id="mainHeader">
+      <h1 className="text-center mt-2 text-4xl font-semibold" id="mainHeader">
         Page not found.
       </h1>
-	  <h1 className="text-center mt-4 text-xl" id="mainHeader">
-        Head back <Link href='/' passHref><span className="text-green-300 
- cursor-pointer">home</span></Link>?
+      <h1 className="text-center mt-4 text-xl" id="mainHeader">
+        Head back{" "}
+        <Link href="/" passHref>
+          <span
+            className="text-green-300 
+ cursor-pointer"
+          >
+            home
+          </span>
+        </Link>
+        ?
       </h1>
     </div>
   );
